28ed91be9983d40610af9a0511e164756ee6af99
Zachuhanez <77264560+Zachuhanez@users.noreply.github.com>
2022-01-23 15:51:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Point.cpp"
+#include <iostream>
 #include <conio.h>
 #include <cstdlib>
 #include <chrono>
@@ -6,63 +6,84 @@
 
 using namespace std;
 
+void Symbol(int L, int arr[][2], int x, int y)
+{
+    for(int i = 1; i < L; i++)
+    {
+        if(arr[i][0] == x && arr[i][1] == y)
+        {
+            cout << '@';
+            break;
+        }
+        if(arr[0][0] == x && arr[0][1] == y)
+        {
+            cout << "*";
+            break;
+        }
+        if(i == L - 1)
+        {
+            cout << " ";
+        }
+    }
+}
+void Draw(int arr[][2], int L, int width = 0, int height = 0)
+{
+    for(int y2 = 0; y2 <= height; y2++)
+    {
+        if (y2 == 0 || y2 == height)
+        {
+            for(int i = 0; i <= width; i++)
+            {
+                cout << "#";
+            }
+            cout << '#' << endl;
+        }
+        else
+        {
+            cout << "#";
+            for(int x2 = 1; x2 <= width; x2++)
+            {
+                Symbol(L, arr, x2, y2);
+            }
+            cout << '#' << endl;
+        }
+    }
+}
+
 int main()
 {
-    system("clear");
-    cout << "      ____  ____         _____  ___   ____\n"
-         << "     |     |    | |\\   |   |   |   \\ |    | |\n"
-         << "     |     |    | | \\  |   |   |___/ |    | |\n"
-         << "     |     |    | |  \\ |   |   |\\    |    | |\n"
-         << "     |____ |____| |   \\|   |   | \\   |____| |____\n"
-         << endl
-         << endl
-         << endl
-         << "                     _____\n"
-         << "                    |     |\n"
-         << "                    |  W  |  -  Up\n"
-         << "               _____|_____|_____\n"
-         << "              |     |     |     |\n"
-         << "              |  A  |  S  |  D  |  -  Right\n"
-         << "              |_____|_____|_____|\n"
-         << "                 |     |\n"
-         << "                 |     |___Down\n"
-         << "                 |\n"
-         << "                 |___Left\n";
-    this_thread::sleep_for(chrono::milliseconds(2800));
-    system("clear");
     srand(time(0));
-
     GAME:
     int NewELement[2];
     char v = ' ', v2 = v;
     int TailL = 5;
-    Point Player(20,20);
-    bool progress = true;
-    int MaxX = 40, MaxY = 40;
+    bool GameOver = false;
+    int MaxX = 25, MaxY = 25;
     int tail[1000][2]{{rand()%MaxX, rand()%MaxY}, {20, 20}, {19, 20}, {18, 20}, {17, 20}};
+    int PlayerX = 20, PlayerY = 20;
 
-    while (true)
+    while (!GameOver)
     {
         system("clear");
-        if(Player.x == MaxX + 1 || Player.y == MaxY || Player.y == 0 || Player.x == 0)
+        if(PlayerX == MaxX || PlayerY == MaxY || PlayerY == 0 || PlayerX == 0)
         {
-            progress = false;
+            GameOver = true;
             break;
         }
 
         for(int i = 2; i < TailL; i++)
         {
-            if(Player.x == tail[i][0])
+            if(PlayerX == tail[i][0])
             {
-                if(Player.y == tail[i][1])
+                if(PlayerY == tail[i][1])
                 {
-                    progress = false;
+                    GameOver = true;
                 }
             }
         }
 
         cout << "Score = " << TailL - 5 << endl;
-        Player.Draw(tail, TailL, MaxX, MaxY);
+        Draw(tail, TailL, MaxX, MaxY);
         if(tail[0][0] == 0 || tail[0][1] == 0)
         {
             if(tail[0][0] == 0)
@@ -87,19 +108,14 @@ int main()
             if(v2 == 's' && v != 'w') { v = 's'; }
             if(v2 == 'a' && v != 'd') { v = 'a'; }
             if(v2 == 'd' && v != 'a') { v = 'd'; }
-            if(v2 == 'q')
-            {
-                progress = false;
-                break;
-            }
         }
 
         this_thread::sleep_for(chrono::milliseconds(150));
 
-        if(v == 'w')      { Player.y--; }
-        else if(v == 's') { Player.y++; }
-        else if(v == 'a') { Player.x--; }
-        else if(v == 'd') { Player.x++; }
+        if(v == 'w')      { PlayerY--; }
+        else if(v == 's') { PlayerY++; }
+        else if(v == 'a') { PlayerX--; }
+        else if(v == 'd') { PlayerX++; }
 
         v2 = v;
 
@@ -112,12 +128,11 @@ int main()
                 tail[i][0] = tail[i-1][0];
                 tail[i][1] = tail[i-1][1];
             }
-            tail[1][0] = Player.x;
-            tail[1][1] = Player.y;
+            tail[1][0] = PlayerX;
+            tail[1][1] = PlayerY;
         }
 
-        //if player eat apple it makes tail longer and create new element
-        if(Player.x == tail[0][0] && Player.y == tail[0][1])
+        if(PlayerX == tail[0][0] && PlayerY == tail[0][1])
         {
             TailL++;
             tail[TailL - 1][0] = NewELement[0];
@@ -126,46 +141,12 @@ int main()
             tail[0][0] = rand()%MaxX;
             tail[0][1] = rand()%MaxY;
         }
-        if(progress == false) { break; }
     }
-    if(!progress)
+    if(GameOver)
     {
         system("clear");
         cout << "Score = " << TailL - 5 << endl;
-        cout << "#####     #     ##   ## #####\n"
-             << "#        # #    # # # # #\n"
-             << "#  ##   #   #   #  #  # #####\n"
-             << "#   #  #######  #     # #\n"
-             << "##### #       # #     # #####\n"
-             << endl
-             << "##### #       # ###### #####\n"
-             << "#   #  #     #  #      #   #\n"
-             << "#   #   #   #   ###### #####\n"
-             << "#   #    # #    #      #  #\n"
-             << "#####     #     ###### #   #\n"
-             << endl
-             << "q - quit | other - restart\n";
-        char s = getch();
-        if(s == 'q') { cout << "Good luck!" << endl; }
-        else         { goto GAME;                    }
-    }
-    else
-    {
-        system("clear");
-        cout << "Score = " << TailL - 5 << endl;
-        cout << "#   # ##### #   #\n"
-             << " # #  #   # #   #\n"
-             << "  #   #   # #   #\n"
-             << "  #   #   # #   #\n"
-             << "  #   #####  ### \n"
-             << endl
-             << "#     #  ##### #   #\n"
-             << "#  #  #  #   # ##  #\n"
-             << "#  #  #  #   # # # #\n"
-             << "# # # #  #   # #  ##\n"
-             << " #   #   ##### #   #\n"
-             << endl
-             << "q - quit | other - restart\n";
+        cout << "#####     #     ##   ## #####\n" << "#        # #    # # # # #\n" << "#  ##   #   #   #  #  # #####\n" << "#   #  #######  #     # #\n" << "##### #       # #     # #####\n" << endl << "##### #       # ###### #####\n" << "#   #  #     #  #      #   #\n" << "#   #   #   #   ###### #####\n" << "#   #    # #    #      #  #\n" << "#####     #     ###### #   #\n" << endl << "q - quit | other - restart\n";
         char s = getch();
         if(s == 'q') { cout << "Good luck!" << endl; }
         else         { goto GAME;                    }
